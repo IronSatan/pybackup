926e40f10707cc550a6c667db5ab732ca8cf4a9b
Matthew Williams <IronSatan@users.noreply.github.com>
2017-09-11 09:09:05 -0600

    Update and rename backup_mfa.py to pybackup.py

--- pybackup.py
+++ pybackup.py
@@ -7,7 +7,7 @@
 # Description: Python script to make backups to /home, /etc, and other directories using rsync in a more user friendly and human readable fashion
 #
 # ToDo:
-# 1. Add runtime flags
+# 1. Add runtime flags (In Progress)
 # 2. Add menu options to chose what directories to add to backup list
 # 3. Add a man page for this command
 # 4. Add use section
